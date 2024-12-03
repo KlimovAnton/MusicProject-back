f7f0352525ac0abd7b44d9505d25be5db3db3fac
Anton <antonklim6@gmail.com>
2024-12-03 19:56:40 +0100

    post

--- src/routers/sendMailRouter.js
+++ src/routers/sendMailRouter.js
@@ -4,6 +4,6 @@ import { sendMailController } from '../controllers/sendMailController.js';
 
 const router = Router();
 
-router.get('/contacts', sendMailController);
+router.post('/contacts', sendMailController);
 
 export default router;
