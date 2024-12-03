1e9a1886be8f05ea174654daa199ef711c0de855
Anton <antonklim6@gmail.com>
2024-12-03 20:52:12 +0100

    query

--- src/controllers/sendMailController.js
+++ src/controllers/sendMailController.js
@@ -1,10 +1,10 @@
 import { sendMail } from "../services/sendMail.js";
 
 export const sendMailController = async (req, res, next) => {
-    const data = await req.body;
+    const data = await req.query;
 
     const send = await sendMail(data);
-
+    console.log(data);
     res.json({
         status: 200,
         message: 'New order',
